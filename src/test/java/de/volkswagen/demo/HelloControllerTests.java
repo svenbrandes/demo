06622051e4d57583875390ee72838b1ea4b521c8
svenbrandes <sven.brandes@me.com>
2021-04-06 11:06:56 +0200

    sayHello_myName_rtnHelloName passed

--- src/test/java/de/volkswagen/demo/HelloControllerTests.java
+++ src/test/java/de/volkswagen/demo/HelloControllerTests.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(HelloController.class)
 public class HelloControllerTests {
@@ -15,9 +17,16 @@ public class HelloControllerTests {
 
     @Test
     void sayHello_noParams_rtnHelloWorld() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().string("Hello World"));
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World"));
+    }
+
+    @Test
+    void sayHello_myName_rtnHelloName() throws Exception {
+        mockMvc.perform(get("/hello?name=Sven"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello Sven"));
     }
 
 }
