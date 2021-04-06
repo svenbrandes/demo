6a83839dcc29990b88328beb059db6a140cb4d10
svenbrandes <sven.brandes@me.com>
2021-04-06 10:58:34 +0200

    sayHello_noParams-rtnHelloWorld passed

---
+++ src/test/java/de/volkswagen/demo/HelloControllerTests.java
@@ -0,0 +1,23 @@
+package de.volkswagen.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(HelloController.class)
+public class HelloControllerTests {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void sayHello_noParams_rtnHelloWorld() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("Hello World"));
+    }
+
+}
